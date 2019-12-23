aaab6dda1e0fd5e71f0c9a9aafcbe218698b4b1a
dossey615 <dossey615@gmail.com>
2019-12-23 12:02:26 +0900

    Initial Commit

---
+++ JinxChecker WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JinxChecker WatchKit Extension
+//
+//  Created by 土居将史 on 2019/12/23.
+//  Copyright © 2019 土居将史. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JinxChecker WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  JinxChecker WatchKit Extension
+//
+//  Created by 土居将史 on 2019/12/23.
+//  Copyright © 2019 土居将史. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
